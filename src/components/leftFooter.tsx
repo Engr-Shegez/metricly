8a64404e28d8b22cbe6c2057de0fe6521584134a
Engr Shegez <adelekeelijah24@gmail.com>
2026-04-10 22:51:13 +0100

    fix: left footer alignment

--- src/components/leftFooter.tsx
+++ src/components/leftFooter.tsx
@@ -3,15 +3,15 @@ import React from "react";
 const LeftFooter = () => {
   return (
     <div>
-      <h1 className="text-orange-500 text-center mb-5 mt-5 text-2xl font-semibold sm:text-left sm:ml-3 sm:mr-0">
+      <h1 className="text-orange-500 text-center mb-5 mt-5 text-2xl font-semibold sm:text-left pr-7 md:pr-0 sm:ml-3 sm:mr-0">
         MetricLy
       </h1>
 
-      <p className="text-4xl font-bold text-center sm:text-left lg:text-left">
+      <p className="text-4xl font-bold pr-7 md:pr-0 text-center  lg:text-left">
         Transform Your Work With Metricly
       </p>
 
-      <p className="text-center sm:text-left sm:ml-4 ml-0 mt-4 text-gray-500 w-sm max-w-md leading-relaxed mx-auto sm:mx-0">
+      <p className="text-center md:text-left  mt-4 text-gray-500 md:pr-0  w-sm max-w-md leading-relaxed mx-auto pr-7 sm:mx-0">
         Transform your business with Metricly. Our platform helps teams turn raw
         data into clear, actionable insights so you can track performance,
         understand trends, and make smarter decisions faster. From real-time
